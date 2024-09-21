2bf71918a9b360b442173eb9dabbf7a16fdae55b
TusharSin810 <tsinghal666@gmail.com>
2024-09-21 17:53:15 +0530

    Token LaunchPad

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Walletadapter from './components/Walletadapter';
+
+function App() {
+  return (
+    <div>
+      <Walletadapter></Walletadapter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Walletadapter.jsx
@@ -0,0 +1,32 @@
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
+import { TokenLaunchpad } from './TokenLaunchpad';
+
+function Walletadapter() {
+  return (
+    <div>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+            <WalletModalProvider>
+              <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: 20
+              }}>
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+              <TokenLaunchpad></TokenLaunchpad>
+            </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  )
+}
+
+export default Walletadapter
